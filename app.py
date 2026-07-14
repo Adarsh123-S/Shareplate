bf2dca760c3edf3e495440ead3aed4122ea4848f
Adarsh123-S <adarshsadarsh06@gmail.com>
2026-07-14 20:49:05 +0530

    fix google oauth scope mismatch

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask_dance.contrib.google import make_google_blueprint, google
 app = Flask(__name__)
 app.secret_key = 'shareplate-secret-key-2024'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
