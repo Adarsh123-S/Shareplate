388eaeb091a102aa60ad239744fe2aeb7268b34e
Adarsh123-S <adarshsadarsh06@gmail.com>
2026-07-20 11:25:36 +0530

    add navbar/footer translations for 6 languages

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
 
 babel = Babel(app, locale_selector=get_locale)
+app.jinja_env.globals['_'] = gettext
 
 @app.route('/set-language/<lang>')
 def set_language(lang):
