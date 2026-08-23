62a0573a594cfa8225b733850607e25214d2998b
Adarsh123-S <adarshsadarsh06@gmail.com>
2026-08-23 15:56:03 +0530

    fixed google oauth scope

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 google_bp = make_google_blueprint(
     client_id=os.environ.get('GOOGLE_CLIENT_ID'),
     client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
-    scope=['profile', 'email'],
+    scope=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
     redirect_url='/google/authorized'
 )
 app.register_blueprint(google_bp, url_prefix='/google')
